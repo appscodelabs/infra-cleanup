d2f486a87f2ad4e26edbe6cf94a4734ff614c3ff
Tamal Saha <tamal@appscode.com>
2020-11-21 00:22:15 -0800

    Fix delete cluster
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/cleanup.sh
+++ hack/scripts/cleanup.sh
@@ -2,12 +2,18 @@
 
 PROJECT=appscode-testing
 
-for name in $(gcloud container clusters list --project=$PROJECT --format='value(name)')
-do
-	gcloud container clusters delete --project=$PROJECT $name # --async
+for entry in $(gcloud container clusters list --project=$PROJECT --format='csv[no-heading](name,zone)'); do
+    IFS=','                                                                            # , is set as delimiter
+    read -ra PARTS <<<"$entry"                                                         # str is read into an array as tokens separated by IFS
+    # echo ${PARTS[0]}
+    # echo ${PARTS[1]}
+    gcloud container clusters delete --project=$PROJECT --zone=${PARTS[1]} ${PARTS[0]} # --async
 done
 
-for name in $(gcloud compute disks list --project=$PROJECT --format='value(name)')
-do
-	gcloud compute disks delete --project=$PROJECT $name
-done
+# for name in $(gcloud container clusters list --project=$PROJECT --format='value(name)'); do
+#     gcloud container clusters delete --project=$PROJECT $name # --async
+# done
+
+# for name in $(gcloud compute disks list --project=$PROJECT --format='value(name)'); do
+#     gcloud compute disks delete --project=$PROJECT $name
+# done
