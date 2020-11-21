c748022725e7516eb899d04d697860bbf55de3ae
Tamal Saha <tamal@appscode.com>
2020-11-21 00:30:47 -0800

    Fix script
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/cleanup.sh
+++ hack/scripts/cleanup.sh
@@ -3,17 +3,13 @@
 PROJECT=appscode-testing
 
 for entry in $(gcloud container clusters list --project=$PROJECT --format='csv[no-heading](name,zone)'); do
-    IFS=','                                                                            # , is set as delimiter
-    read -ra PARTS <<<"$entry"                                                         # str is read into an array as tokens separated by IFS
-    # echo ${PARTS[0]}
-    # echo ${PARTS[1]}
-    gcloud container clusters delete --project=$PROJECT --zone=${PARTS[1]} ${PARTS[0]} # --async
+    IFS=','                                                                                    # , is set as delimiter
+    read -ra PARTS <<<"$entry"                                                                 # str is read into an array as tokens separated by IFS
+    gcloud container clusters delete --quiet --project=$PROJECT --zone=${PARTS[1]} ${PARTS[0]} # --async
 done
 
-# for name in $(gcloud container clusters list --project=$PROJECT --format='value(name)'); do
-#     gcloud container clusters delete --project=$PROJECT $name # --async
-# done
-
-# for name in $(gcloud compute disks list --project=$PROJECT --format='value(name)'); do
-#     gcloud compute disks delete --project=$PROJECT $name
-# done
+for entry in $(gcloud compute disks list --project=$PROJECT --format='csv[no-heading](name,zone)'); do
+    IFS=','                    # , is set as delimiter
+    read -ra PARTS <<<"$entry" # str is read into an array as tokens separated by IFS
+    gcloud compute disks delete --quiet --project=$PROJECT --zone=${PARTS[1]} ${PARTS[0]}
+done
