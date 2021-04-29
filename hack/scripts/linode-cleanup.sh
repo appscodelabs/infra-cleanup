c2e620c4731047a3841ffac55944f514b31fccb6
Tamal Saha <tamal@appscode.com>
2021-04-29 00:24:12 -0700

    Add messages
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/linode-cleanup.sh
+++ hack/scripts/linode-cleanup.sh
@@ -2,8 +2,10 @@
 
 set -eou pipefail
 
+echo "linode-cli version:"
 linode-cli --version
 
+echo "The following volumes are unattached and will be deleted:"
 linode-cli volumes list --json --pretty | jq -r '.[]| select (.linode_id == null) | .id'
 
 for vol in $(linode-cli volumes list --json --pretty | jq -r '.[]| select (.linode_id == null) | .id'); do
