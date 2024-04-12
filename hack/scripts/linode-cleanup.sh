1f3a2fd2aec2922b2f18917e9d4e88c46407e504
Tamal Saha <tamal@appscode.com>
2024-04-12 01:43:19 -0700

    Skip ninja and prod volumes
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/linode-cleanup.sh
+++ hack/scripts/linode-cleanup.sh
@@ -5,10 +5,30 @@ set -eou pipefail
 echo "linode-cli version:"
 linode-cli --version
 
-echo "The following volumes are unattached and will be deleted:"
-linode-cli volumes list --json --pretty | jq -r '.[]| select (.linode_id == null) | .id'
+# echo "The following volumes are unattached and will be deleted:"
+# linode-cli volumes list --json --pretty | jq -r '.[]| select (.linode_id == null) | .id'
+
+# ninja cluster
+linode-cli lke kubeconfig-view 19953 --json | jq -r '.[0].kubeconfig' | base64 -d >/tmp/kubeconfig_qa
+qa_vols=$(kubectl --kubeconfig=/tmp/kubeconfig_qa get pv -o=jsonpath='{range .items[?(@.status.phase=="Bound")]}{.spec.csi.volumeHandle}{"\n"}{end}' | cut -d '-' -f 1 | sort)
+# echo $qa_vols
+
+# prod cluster
+linode-cli lke kubeconfig-view 25516 --json | jq -r '.[0].kubeconfig' | base64 -d >/tmp/kubeconfig_prod
+prod_vols=$(kubectl --kubeconfig=/tmp/kubeconfig_prod get pv -o=jsonpath='{range .items[?(@.status.phase=="Bound")]}{.spec.csi.volumeHandle}{"\n"}{end}' | cut -d '-' -f 1 | sort)
+# echo $prod_vols
 
 for vol in $(linode-cli volumes list --json --pretty | jq -r '.[]| select (.linode_id == null) | .id'); do
+	if [[ ${qa_vols[@]} =~ $vol ]]; then
+		echo "$vol found in ninja and skipped"
+		continue
+	fi
+	if [[ ${prod_vols[@]} =~ $vol ]]; then
+		echo "$vol found in prod and skipped"
+		continue
+	fi
+
+	echo "$vol will be deleted"
 	linode-cli volumes delete $vol || true
 	sleep 1
 done
